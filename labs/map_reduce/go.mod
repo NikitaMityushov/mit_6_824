d1d65a737a64376dc29bfdcdbaa2a633fca47727
NikitaMityushov <nikitamityushov@gmail.com>
2024-07-27 11:03:21 +0300

    #5 added skeleton and files for map_reduce lab

---
+++ labs/map_reduce/go.mod
@@ -0,0 +1,3 @@
+module github.com/NikitaMityushov/mit_6_824/labs/map_reduce
+
+go 1.22.2
---
+++ labs/map_reduce/main/run_worker.sh
@@ -0,0 +1,5 @@
+go build -buildmode=plugin ./mrapps/wc.go
+go run mrworker.go wc.so &
+go run mrworker.go wc.so &
+go run mrworker.go wc.so &
+go run mrworker.go wc.so &
